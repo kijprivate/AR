a9a41ca5ee8cb7ae41da455a841ebdfbf7ddc057
Jakub Broda <jakub.broda13@gmail.com>
2020-10-04 17:39:35 +0200

    OnGuiTesting

---
+++ Assets/_MyAssets/OnGuiTesting.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnGuiTesting : MonoBehaviour
+{
+    #region Singleton
+    private static OnGuiTesting instance;
+    public static OnGuiTesting Instance
+    {
+        get
+        {
+            if (instance)
+                return instance;
+            else
+                return FindObjectOfType<OnGuiTesting>();
+        }
+    }
+    #endregion
+
+    public string message1;
+    public string message2;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void OnGUI()
+    {
+        GUI.skin.label.fontSize = 50;
+        GUI.Label(new Rect(30, 30, 1000, 1000), message1);
+        GUI.Label(new Rect(30, 80, 1000, 1000), message2);
+    }
+}
