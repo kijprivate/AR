a1571eb026aa2b8d4a3ed92cbde4fa695e967ea5
Jakub Broda <jakub.broda13@gmail.com>
2020-10-08 21:21:29 +0200

    trigger player

--- Assets/_MyAssets/TriggerPlayerInPoint.cs
+++ Assets/_MyAssets/TriggerPlayerInPoint.cs
@@ -6,9 +6,6 @@ public class TriggerPlayerInPoint : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Marker"))
-        {
-            OnGuiTesting.Instance.message1 = other.gameObject.GetComponent<Marker>().message;
-        }
+
     }
 }
